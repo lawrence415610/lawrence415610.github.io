81fe554705096b903818ed08d9853b9da0373b46
lawrence415610 <lawrence415610@gmail.com>
2020-03-07 17:09:44 +1100

    add a new one

--- js/main.js
+++ js/main.js
@@ -1,90 +1,52 @@
-; (function () {
+window.addEventListener('load', function () {
 
-    var fullHeight = function () {
-        $('.js-fullheight').css('height', $(window).height());
-        $(window).resize(function () {
-            $('.js-fullheight').css('height', $(window).height());
-        });
-    };
+    const nav = document.querySelectorAll('#nav li');
+    const section = document.querySelectorAll('section');
 
-    $('#navigation').find('a').click(function(){
-        //为了被chrome和firefox支持，所以要写html和body
-        $("html, body").animate({scrollTop: $($(this).attr("href")).offset().top -20+ "px"}, 500);
-        return false;
-    });
+    const navPortfolio = document.querySelectorAll('#works li');
+    const navPortfolioLabel = document.querySelectorAll('#works li>a');
+    const portfolioWorks = document.querySelectorAll('#works .work_wrapper');
 
-    var navActive = function(section){
-        $('#navigation').find('li').removeClass('active');
-        $('#navigation').find('li').each(function(){
-            $(this).find('a[data-nav="'+section+'"]').closest('li').addClass('active');
+    // TODO: use the preloader to load me img
+
+
+    // general navigation, click the nav bar to show the three sections
+    for (let i = 0, len = nav.length; i < len; i++) {
+        nav[i].addEventListener('click', () => {
+            //clear all before set
+            nav.forEach((each) => {
+                each.classList.remove('li_current');
+            });
+            section.forEach((each) => {
+                each.classList.remove('show_section');
+            });
+            //set the clicked one a new class
+            nav[i].classList.add('li_current');
+            section[i].classList.add('show_section');
         });
     }
 
-    var navigationSection = function() {
-    var $section = $('section[data-section]');
-    $section.waypoint(function(direction) {
-        if (direction === 'down') {
-            navActive($(this.element).data('section'));
-        }
-    }, {
-        offset: '150px'
+    // portfolio navigation, click the tab then show the work with the label
+    navPortfolio.forEach((el1) => {
+        el1.addEventListener('click', function () {
+            // change the state of the label
+            let label = el1.firstElementChild;
+            navPortfolioLabel.forEach((el2) => {
+                el2.classList.remove('work_current');
+            });
+            label.classList.add('work_current');
+            // check whether the data-type of the nav and the element are the same, if same, show, otherwise hide
+            portfolioWorks.forEach((el3) => {
+                if (label.getAttribute('data-type') === el3.getAttribute('data-type')) {
+                    el3.style.display = 'block';
+                } else if (label.getAttribute('data-type') === 'all') {
+                    portfolioWorks.forEach((el4) => {
+                        el4.style.display = 'block';
+                    })
+                } else {
+                    el3.style.display = 'none';
+                }
+            })
+        });
     });
-    $section.waypoint(function(direction) {
-        if (direction === 'up') {
-            navActive($(this.element).data('section'));
-        }
-    }, {
-        offset: function() { return -$(this.element).height() + 155; }
-    });
-};
-
-	var addAnimation = function() {
-		var i = 0;
-		$('.animation').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animation.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated');
-							} else {
-								el.addClass('fadeInUp animated');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '85%' } );
-	};
-
-
-
-
-    
-
-    $(function () {
-        fullHeight();
-        navigationSection();
-        addAnimation();
-    });
-
-
-
-}());
+});
